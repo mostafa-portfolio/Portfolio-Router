047f945318c468e16a485cb2d6d84ebcb9557e60
Mostafa <Mo.Mohammed@nu.edu.eg>
2026-09-23 19:24:28 +0300

    Lower header style changes 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom"
 export default function Header(props){
     return(
         <header className="header"> 
-            <div><Link to='/' className={`yellow-highlight ${props.aboutClass}`}>About</Link></div>
-            <div><Link to='/resume' className={`yellow-highlight ${props.resumeClass}`}>Resume</Link></div>
-            <div><Link to='/portfolio' className={`yellow-highlight ${props.portfolioClass}`}>Projects</Link></div>
-            <div><Link to='/certificates' className={`yellow-highlight ${props.certificatesClass}`}>Certificates</Link></div>
+            <div><Link to='/' className={`yellow-highlight ${props.aboutClass}`}><span className="lower-header">About</span></Link></div>
+            <div><Link to='/resume' className={`yellow-highlight ${props.resumeClass}`}><span className="lower-header">Resume</span></Link></div>
+            <div><Link to='/portfolio' className={`yellow-highlight ${props.portfolioClass}`}><span className="lower-header">Projects</span></Link></div>
+            <div><Link to='/certificates' className={`yellow-highlight ${props.certificatesClass}`}><span className="lower-header">Certificates</span></Link></div>
         </header>
     )
 }
